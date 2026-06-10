bc612b8638d853b08207c06762efa51a11d75221
Stephan Schlöpke <stephan@schloepke.de>
2026-06-10 11:23:28 +0200

    Changeset: 457 GameEventScript: Formatting

--- BeamableServices/services/StepH-GameEventScript/GameEventScriptManager.cs
+++ BeamableServices/services/StepH-GameEventScript/GameEventScriptManager.cs
@@ -9,15 +9,21 @@ namespace StepH.GameEventScript;
 
 public static class GameEventScriptManager
 {
-    public static GameEventScriptBuilder CreateScriptBuilder() => GameEventScriptBuilder.Create();
+    public static GameEventScriptBuilder CreateScriptBuilder()
+        => GameEventScriptBuilder.Create();
 
-    public static GameEventScriptCompiled Compile(string input, GameEventScriptCompileOptions? options = null) => CreateScriptBuilder().AddScript(input).Compile(options);
+    public static GameEventScriptCompiled Compile(string input, GameEventScriptCompileOptions? options = null)
+        => CreateScriptBuilder().AddScript(input).Compile(options);
 
-    public static IGameEventScriptModule CompileModuleOldVm(string input, GameEventScriptCompileOptions? options = null) => CreateScriptBuilder().AddScript(input).CompileModule(options);
-    public static IGameEventScriptModule CompileModule(string input, GameEventScriptCompileOptions? options = null) => 
-        GameEventScriptVirtualMaschine.Create(CreateScriptBuilder().AddScript(input).Compile(options).ToGameEventScriptBinary(), 128, 128);
-    public static IGameEventScriptModule CreateModuleNewVm(GameEventScriptBinary binary, ushort registerSize = 128, ushort stackSize = 128) =>
-        GameEventScriptVirtualMaschine.Create(binary, registerSize, stackSize);
+    public static IGameEventScriptModule CompileModuleOldVm(string input, GameEventScriptCompileOptions? options = null)
+        => CreateScriptBuilder().AddScript(input).CompileModule(options);
+
+    public static IGameEventScriptModule CompileModule(string input, GameEventScriptCompileOptions? options = null)
+        => GameEventScriptVirtualMaschine.Create(CreateScriptBuilder().AddScript(input).Compile(options).ToGameEventScriptBinary(), 128, 128);
+
+    public static IGameEventScriptModule CreateModuleNewVm(GameEventScriptBinary binary, ushort registerSize = 128, ushort stackSize = 128)
+        => GameEventScriptVirtualMaschine.Create(binary, registerSize, stackSize);
     
-    public static GameEventScriptHostBuilder CreateHostBuilder() => GameEventScriptHost.CreateBuilder();
+    public static GameEventScriptHostBuilder CreateHostBuilder()
+        => GameEventScriptHost.CreateBuilder();
 }
